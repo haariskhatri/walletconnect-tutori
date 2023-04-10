012dcf38e2d369e5298b2f2e9bdf516d82fd4426
haariskhatri <haarishkkhatri@gmail.com>
2023-04-10 20:46:08 +0530

    App

--- login-app/src/App.jsx
+++ login-app/src/App.jsx
@@ -1,36 +1,35 @@
 import { useEffect,useState } from 'react'
-import {SignClient} from '@walletconnect/sign-client';
+import Axios from 'axios'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const[signClient,setSignClient] = useState();
 
-  async function createClient(){
-    try {
-      const client = await SignClient.init({
-        projectId: '8a54dac1f7ec8a35a6663cf03dd49d4a'
-      })
-      //console.log("Client" , client);
-      setSignClient(client);
-    } catch (error) {
-      console.log(error)
-    }
+  const [joke,setJoke] = useState("");
+  
+  const getJoke = ()=>{
+    fetch("https://app.ajrakhhouse.com:4000/article",{
+      method: "POST",
+      body: JSON.stringify({
+        url:'https://www.indiatoday.in/auto/latest-auto-news/story/mg-comet-ev-launch-company-collaborates-with-a-gamer-for-introducing-electric-model-2358248-2023-04-10'
+      }),
+    }).then((response)=>{
+      return response.json()
+      // setJoke(response.data.setup + '....' + response.data.punchline);
+    }).then((data)=>{
+      console.log(data)
+    })
   }
-
-  useEffect(()=>{
-    if(!signClient){
-      createClient();
-    }
-  },[signClient])
 
   return (
     <div className="App">
       <div>
         <h1>Hello</h1>
+        <button onClick={getJoke}>Get Joke</button>
+        {joke}
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
